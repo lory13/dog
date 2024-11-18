ac573212c411b2a7378a859b37677b0cd288573b
Mike Lory <m.joseph.lory@gmail.com>
2024-11-18 12:43:48 -0600

    second commit

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "When the puppy grows up it will say: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lory13/dog
+
+go 1.23.0
